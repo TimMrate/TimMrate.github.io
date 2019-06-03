e72d6f58410d92f097183ae2bc269246bf5849d3
TimMrate <50410928+TimMrate@users.noreply.github.com>
2019-06-03 13:53:57 +0300

    header complite

--- Digital Crafters/js/script.js
+++ Digital Crafters/js/script.js
@@ -1,7 +1,11 @@
 
-    var $hamburger = $(".hamburger");
-  $hamburger.on("click", function(e) {
-    $hamburger.toggleClass("is-active");
-    // Do something else, like open/close menu
-  });
 
+$( document ).ready(function() {
+  var menu = $('.menu');
+    var $hamburger = $(".hamburger");
+    $hamburger.on("click", function(e) {
+      $hamburger.toggleClass("is-active");
+      menu.toggleClass('active-menu')
+      // Do something else, like open/close menu
+    });
+});
