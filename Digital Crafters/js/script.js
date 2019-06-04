8d5cd833ce3972099f777d9774f951d86f9f0b51
TimMrate <50410928+TimMrate@users.noreply.github.com>
2019-06-04 22:00:08 +0300

    section 2

--- Digital Crafters/js/script.js
+++ Digital Crafters/js/script.js
@@ -5,7 +5,32 @@ $( document ).ready(function() {
     var $hamburger = $(".hamburger");
     $hamburger.on("click", function(e) {
       $hamburger.toggleClass("is-active");
-      menu.toggleClass('active-menu')
       // Do something else, like open/close menu
+      menu.toggleClass('active-menu');
     });
+    // Owl carousel
+    var owl1 = $("#review-slider");
+      owl1.owlCarousel({
+        center: true,
+        
+        nav: true,
+        navText: ['<span class="prev"><img src="img/left.png" alt="prev" srcset=""></span>','<span class="next"><img src="img/right.png" alt="next" srcset=""></span>'],
+        smartSpeed: 800,
+        responsive:{
+          0:{
+            items:1
+          },
+          768:{
+            items:1
+          },
+          1180:{
+            items: 1,
+          },
+          1660:{
+            items: 2,
+            margin: 40,
+          }
+        }
+      });
+    
 });
