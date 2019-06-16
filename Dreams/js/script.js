6b2f4c121d297a7a6eae67ace2107b891150988b
TimMrate <50410928+TimMrate@users.noreply.github.com>
2019-06-16 22:16:41 +0300

    щцд

--- Dreams/js/script.js
+++ Dreams/js/script.js
@@ -1,22 +1,57 @@
-$( document ).ready(function() {
-    // Menu
-    var menu = $('#hmenu'),
-        hamburger = $('.hamburger');
-    hamburger.on("click", function(e) {
+$(document).ready(function() {
+  // Menu
+  var menu = $("#hmenu"),
+    hamburger = $(".hamburger");
+  hamburger.on("click", function(e) {
     hamburger.toggleClass("is-active");
-        // Do something else, like open/close menu
-        menu.toggleClass('active-menu');
-        $('html').toggleClass('overflow');
-      });
-      // Tabs
-      $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
-        $(this)
-        .addClass('active').siblings().removeClass('active')
-        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
-      });
-      // Owl carousel
-      var owl = $('');
-        owl.owlCarousel({
-          
-        });
+    // Do something else, like open/close menu
+    menu.toggleClass("active-menu");
+    $("html").toggleClass("overflow");
   });
+  // Tabs
+  $("ul.tabs__caption").on("click", "li:not(.active)", function() {
+    $(this)
+      .addClass("active")
+      .siblings()
+      .removeClass("active")
+      .closest("div.tabs")
+      .find("div.tabs__content")
+      .removeClass("active")
+      .eq($(this).index())
+      .addClass("active");
+  });
+  // Owl carousel
+  var owl = $("#places");
+  owl.owlCarousel({
+    startPosition: 3,
+    responsive:{
+      0:{
+        items:1,
+      },
+      768:{
+        items:1,
+      },
+      1180:{
+        items: 2,
+      },
+      1660:{
+        items: 3,
+      }
+    }
+  });
+  // Якорные ссылки
+  $(".link").on("click", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 2000);
+});
+$(".link-menu").on("click", function (event) {
+  event.preventDefault();
+  var id  = $(this).attr('href'),
+      top = $(id).offset().top;
+  $('body,html').animate({scrollTop: top}, 2000);
+  menu.toggleClass('active-menu');
+  $hamburger.toggleClass("is-active");
+});
+});
