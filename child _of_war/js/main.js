c60e019dff82705789929a55838a448943325d1f
TimMrate <50410928+TimMrate@users.noreply.github.com>
2019-11-16 22:06:42 +0300

    commit
    SDG

--- child _of_war/js/main.js
+++ child _of_war/js/main.js
@@ -1,23 +1,23 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        nav:true,
+        loop: true,
+        nav: true,
         navText: ['<span><i class="fas fa-chevron-left"></i></span>',
-      '<span><i class="fas fa-chevron-right"></i></span>'],
-       items: 1,
-       center:true,
-       dots: true,
-       margin: 180,
-       smartSpeed: 800, 
-       autoplay: true, 
-       autoplayHoverPause: true,
+            '<span><i class="fas fa-chevron-right"></i></span>'],
+        items: 1,
+        center: true,
+        dots: true,
+        margin: 180,
+        smartSpeed: 800,
+        autoplay: true,
+        autoplayHoverPause: true,
     });
     var $page = $('html, body');
-$('a[href*="#"]').click(function() {
-    $page.animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 800);
-    return false;
+    $('a[href*="#"]').click(function () {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 800);
+        return false;
+    });
+    $('.parallax-window').parallax({ imageSrc:'../img/bg.png'});
 });
-  });
-  
