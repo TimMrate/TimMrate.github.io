e294cbbb416709ca2c1667df09775a0dd119353e
TimMrate <50410928+TimMrate@users.noreply.github.com>
2019-11-16 20:22:22 +0300

    First
    dag

---
+++ child _of_war/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        nav:true,
+        navText: ['<span><i class="fas fa-chevron-left"></i></span>',
+      '<span><i class="fas fa-chevron-right"></i></span>'],
+       items: 1,
+       center:true,
+       dots: true,
+       margin: 180,
+       smartSpeed: 800, 
+       autoplay: true, 
+       autoplayHoverPause: true,
+    });
+    var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 800);
+    return false;
+});
+  });
+  
