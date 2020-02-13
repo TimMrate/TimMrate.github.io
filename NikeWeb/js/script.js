f8c986509ec3964408247021615f556d9ab968e5
TimMrate <50410928+TimMrate@users.noreply.github.com>
2020-02-13 19:35:16 +0300

    2
    2

--- NikeWeb/js/script.js
+++ NikeWeb/js/script.js
@@ -1,12 +1,13 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-      items:1,
-      animateOut: 'IN',
-      animateIn: 'OUT', 
-      // startPosition: 3,
-      dots: true,
-      dotsContainer: "#owl-dots",
-      mouseDrag: false,
-    });
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    animateOut: "IN",
+    animateIn: "OUT",
+    // startPosition: 3,
+    dots: true,
+    dotsContainer: "#owl-dots",
+    mouseDrag: false,
+    pullDrag: false
   });
- 
+  
+});
